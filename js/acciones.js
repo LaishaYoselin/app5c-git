3db0d0a004af4e915862e1306e72e9cd3e030394
LaishaYoselin <laishayose07@gmail.com>
2017-05-03 07:29:27 -0500

    2s

--- js/acciones.js
+++ js/acciones.js
@@ -1,17 +1,15 @@
-// JavaScript
+// JavaScript Document
 
-$(document).ready(function(e){
-	document.addEventListener("deviceready",function() {
-		
-		$('#beep').tap(function(){
-			navigator.notification.beep(1);
-		});//tap bepp
-		
-		$('#vibrar').tap(function(){
-			navigator.notification.vibrate(1000);
-		});//tap vibrar
-		
+$ (document). ready(function(e){
+	document.addEventListener("deviceready",function(){
+  
+$ ('#beep').tap(function(){
+	navigator.notification.beep(1);
+});//tap beep
+
+$('#vibrar').tap(function(){
+	navigator.notification.vibrate(1000);
+});// tap vibrar
+
 	},false); //deviceready
 }); //ready
-			
-		
